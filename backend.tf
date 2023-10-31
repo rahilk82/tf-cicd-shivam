3daf3f12c747952ca2d2b07dfb046af348b28648
Rahil Khan <77859920+rahilk82@users.noreply.github.com>
2023-10-31 12:13:31 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,6 +0,0 @@
-backend "azurerm" {
-            resource_group_name  = "tfstate-rg"
-            storage_account_name = "tfstatex4rf1"
-            container_name       = "tfstate-container"
-            key                  = "terraform.tfstate"
-  }
